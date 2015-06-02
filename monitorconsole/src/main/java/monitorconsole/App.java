9dda62f721d2d4658b314d0633a21db883fe45b6
Artur Kiszka <artur_kiszka@wp.pl>
2015-06-02 16:10:37 +0200

    Struktura console

---
+++ monitorconsole/src/main/java/monitorconsole/App.java
@@ -0,0 +1,13 @@
+package monitorconsole;
+
+/**
+ * @author Artur Kiszka
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Struktura projektu" );
+    }
+}
