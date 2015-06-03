61c4875de7bfcbea9bd15b6a8fcd95b5e58f9c22
obserwator <matt2mad@gmail.com>
2015-06-03 19:01:44 +0200

    Rozszerzenie UI o parametry sortowania

--- monitorconsole/src/main/java/monitorconsole/ConsoleUI.java
+++ monitorconsole/src/main/java/monitorconsole/ConsoleUI.java
@@ -20,44 +20,69 @@ public class ConsoleUI extends JFrame implements ActionListener {
 	private JRadioButton sortByHostIp;
 	private JRadioButton sortByMeasurement;
 	
+	private JRadioButton sortAscending;
+	private JRadioButton sortDescending;
+	
     public ConsoleUI() {
         initUI();
     }
 
     private void initUI() {
-        
     	textArea = new JTextArea(13, 60);
     	textArea.setFont(new Font("monospaced", Font.PLAIN, 12));
-    	scrollPane = new JScrollPane(textArea); 
     	textArea.setEditable(false);
+    	scrollPane = new JScrollPane(textArea); 
     	
     	cpuButton = new JRadioButton("CPU");
     	cpuButton.setActionCommand("CPU");
-    	cpuButton.setSelected(true);
-    	currentMeasurementType = "CPU";
-
     	memButton = new JRadioButton("Memory");
     	memButton.setActionCommand("Memory");
-
     	upButton = new JRadioButton("Network Up");
     	upButton.setActionCommand("Network Up");
-    	
     	downButton = new JRadioButton("Network Down");
-    	downButton.setMnemonic(KeyEvent.VK_DOWN);
     	downButton.setActionCommand("Network Down");
+
+    	cpuButton.setSelected(true);
+    	currentMeasurementType = "CPU";
     	
     	cpuButton.addActionListener(this);
     	memButton.addActionListener(this);
     	upButton.addActionListener(this);
     	downButton.addActionListener(this);
     	
-    	JRadioButton[] buttons = {cpuButton,memButton,upButton,downButton};
-    	ButtonGroup group = new ButtonGroup();
+    	JRadioButton[] measurementTypeButtons = {cpuButton,memButton,upButton,downButton};
+    	ButtonGroup measurementTypeGroup = new ButtonGroup();
     	
-    	for(JRadioButton button : buttons)
-    		group.add(button);
+    	for(JRadioButton button : measurementTypeButtons)
+    		measurementTypeGroup.add(button);
     	
-    	createLayout(scrollPane, buttons); 
+    	sortByHostName = new JRadioButton("by host name");
+    	sortByHostName.setActionCommand("by host name");
+    	sortByHostIp = new JRadioButton("by host IP");
+    	sortByHostIp.setActionCommand("by host IP");
+    	sortByMeasurement = new JRadioButton("by measurement");
+    	sortByMeasurement.setActionCommand("by measurement");
+    	sortByMeasurement.setSelected(true);
+    	
+    	JRadioButton[] sortKeyButtons = {sortByHostName,sortByHostIp,sortByMeasurement};
+    	ButtonGroup sortKeyGroup = new ButtonGroup();
+    	
+    	for(JRadioButton button : sortKeyButtons)
+    		sortKeyGroup.add(button);
+    	
+    	sortAscending = new JRadioButton("ascending");
+    	sortAscending.setActionCommand("ascending");
+    	sortDescending = new JRadioButton("descending");
+    	sortDescending.setActionCommand("descending");
+    	sortDescending.setSelected(true);
+    	
+    	JRadioButton[] sortTypeButtons = {sortAscending,sortDescending};
+    	ButtonGroup sortTypeGroup = new ButtonGroup();
+    	
+    	for(JRadioButton button : sortTypeButtons)
+    		sortTypeGroup.add(button);
+    	
+    	createLayout(scrollPane, measurementTypeButtons, sortKeyButtons, sortTypeButtons); 
         
         setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -65,18 +90,42 @@ public class ConsoleUI extends JFrame implements ActionListener {
         //setSize(600, 300);
     }
     
-    private void createLayout(JScrollPane scrollPane, JRadioButton[] buttons) {
+    private void createLayout(JScrollPane scrollPane, JRadioButton[] measurementTypeButtons, JRadioButton[] sortKeyButtons, JRadioButton[] sortTypeButtons) {
         
-        JPanel pane = (JPanel) getContentPane();
-        JPanel controlsPane = new JPanel();
-        BorderLayout gl = new BorderLayout();
-        pane.setLayout(gl);
+        JPanel mainPane = (JPanel) getContentPane();
+        JPanel buttonPane = new JPanel();
         
-        for(JRadioButton button : buttons)
-        	controlsPane.add(button);
+        JPanel measurementTypePane = new JPanel();
+        JPanel sortTypePane = new JPanel();
         
-        pane.add(controlsPane, BorderLayout.NORTH);
-        pane.add(scrollPane, BorderLayout.SOUTH);
+        mainPane.setLayout(new BorderLayout());
+        buttonPane.setLayout(new BorderLayout());
+        measurementTypePane.setLayout(new GridLayout(2, 4));
+        sortTypePane.setLayout(new GridLayout(3, 3));
+        
+        measurementTypePane.add(new JLabel("Measurement"));
+        measurementTypePane.add(new JLabel(""));
+        measurementTypePane.add(new JLabel(""));
+        measurementTypePane.add(new JLabel(""));
+        for(JRadioButton button : measurementTypeButtons)
+        	measurementTypePane.add(button);
+        
+        sortTypePane.add(new JLabel("Sort"));
+        sortTypePane.add(new JLabel(""));
+        sortTypePane.add(new JLabel(""));
+        
+        sortTypePane.add(new JLabel(""));
+        for(JRadioButton button : sortTypeButtons)
+        	sortTypePane.add(button);
+        
+        for(JRadioButton button : sortKeyButtons)
+        	sortTypePane.add(button);
+        
+        buttonPane.add(measurementTypePane, BorderLayout.NORTH);
+        buttonPane.add(sortTypePane, BorderLayout.SOUTH);
+        
+        mainPane.add(buttonPane, BorderLayout.NORTH);
+        mainPane.add(scrollPane, BorderLayout.SOUTH);
         pack();        
     }
     
